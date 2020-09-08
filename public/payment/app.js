a6192e40ce91ea4a39154956fcd6f6e895161f8a
portableD <emmanueltemitopedorcas20@gamil.com>
2020-09-08 21:31:54 +0100

    save

--- public/payment/app.js
+++ public/payment/app.js
@@ -1,20 +1,21 @@
 function payWithPaystack() {
 let handler = PaystackPop.setup ({
     key:"pk_test_82ce23694563611af6015b7bdfc1dd4a1f044acf",
-    email:"emmanueltemitopedorcas20@gmail.com",
+    email: "emmanueldorcas15@gmail.com",
     amount: 370000,
     metadata: {
         custom_fields:[
             {
                 display_name: "Mobile Number",
                 variable_name: "mobile_number",
-                value:"+2349078562988",
+                value:document.querySelector('.phone').value,
             }
         ]
     },
-    callback: function (response){
-        alert("success. transaction ref is" + response);
-    },
+    callback: function(response){
+        let message = 'Payment complete! Reference: ' + response.reference;
+        alert(message);
+      },
     onClose: function () {
         alert("Transaction cancelled");
     }
